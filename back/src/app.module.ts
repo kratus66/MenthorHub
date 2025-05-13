efcf3edb1920725f17f6b153b98f1e33ef3e4169
Nass90100 <nwh250@hotmail.com>
2025-05-13 07:41:15 -0400

    Agregado .gitignore global y estructura base backend

---
+++ back/src/app.module.ts
@@ -0,0 +1,9 @@
+// filepath: /Users/gabyaybar/Desktop/PF/MentorHub-PF/back/src/app.module.ts
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
