d4cb503183096ebc8764587adb11a5becbe59fe2
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-05-14 19:46:54 -0300

    Pagina dashboard a medio hacer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './views/Home/Home';
-import Login from './components/LoginForm/LoginForm';
+import Home from './views/Home/home';
+import Login from './views/Login/Login';
 import Usuario from './views/Usuario/Usuario';
 import Clases from './views/Clases/Clases';
 import AboutUs from './views/AboutUs/AboutUs';
 import NavBar from './components/Navbar/Navbar';
 import Landing from './views/Landing/Landing';
 import Register from './views/Register/Register';
+import Dashboard from './views/Dashboard/Dashboard';
 
 const App = () => {
-  return (
-    <Router>
-      {/* Usamos la propiedad pathname del window.location para determinar si mostrar el NavBar */}
-      {window.location.pathname !== '/login' && window.location.pathname !== '/register' && <NavBar />}
-      
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/usuario" element={<Usuario />} />
-        <Route path="/clases" element={<Clases />} />
-        <Route path="/about-us" element={<AboutUs />} />
-      </Routes>
-    </Router>
-  );
+   return (
+      <Router>
+         {/* Usamos la propiedad pathname del window.location para determinar si mostrar el NavBar */}
+         {window.location.pathname !== '/login' &&
+            window.location.pathname !== '/register' && <NavBar />}
+
+         <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/usuario" element={<Usuario />} />
+            <Route path="/clases" element={<Clases />} />
+            <Route path="/panel" element={<Dashboard />} />
+            <Route path="/about-us" element={<AboutUs />} />
+         </Routes>
+      </Router>
+   );
 };
 
 export default App;
