cc0e91e235c73c51db458392d04c8840fd720309
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-05-15 16:48:27 -0300

    arreglo de urls en helpers

--- front/src/helpers/categorias.ts
+++ front/src/helpers/categorias.ts
@@ -3,23 +3,23 @@ import type { Categoria } from '../types/Categoria';
 const categorias_array: Categoria[] = [
    {
       nombre: 'Musica',
-      imagen: './public/categoria-musica.png',
+      imagen: '/categoria-musica.png',
    },
    {
       nombre: 'Matemática',
-      imagen: './public/categoria-matematica.png',
+      imagen: '/categoria-matematica.png',
    },
    {
       nombre: 'Ciencia',
-      imagen: './public/categoria-ciencia.jpg',
+      imagen: '/categoria-ciencia.jpg',
    },
    {
       nombre: 'Finanzas',
-      imagen: './public/categoria-finanzas.jpg',
+      imagen: '/categoria-finanzas.jpg',
    },
    {
       nombre: 'Lenguages',
-      imagen: './public/categoria-lenguajes.webp',
+      imagen: '/categoria-lenguajes.webp',
    },
 ];
 
