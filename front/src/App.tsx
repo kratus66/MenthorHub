7b0a09ba211f61827a4d650e0c4bec8ff5c2499c
BDtobio <tobiasbustosdiaco989@gmail.com>
2025-05-16 12:36:48 -0300

    cambios

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './views/Home/home';
 import Login from './views/Login/Login';
 import Usuario from './views/Usuario/Usuario';
@@ -9,23 +9,48 @@ import Landing from './views/Landing/Landing';
 import Register from './views/Register/Register';
 import Dashboard from './views/Dashboard/Dashboard';
 
+import Terminos from './views/terminos-condiciones/Terminos';
+import Layout from './components/layout/Layout';
+
+const AppWrapper = () => {
+  
+  const location = useLocation();
+  const noNavFooter = ['/login', '/register', '/'].includes(location.pathname);
+
+  return (
+    <>
+      {!noNavFooter && <NavBar />}
+
+      {noNavFooter ? (
+        
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      ) : (
+        // Para las demás rutas envuelves en Layout (footer incluido ahí)
+        <Layout>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/usuario" element={<Usuario />} />
+            <Route path="/clases" element={<Clases />} />
+            <Route path="/panel" element={<Dashboard />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/terminos" element={<Terminos />} />
+          </Routes>
+        </Layout>
+      )}
+
+      
+    </>
+  );
+};
+
 const App = () => {
   return (
     <Router>
-      
-      {window.location.pathname !== '/login' && window.location.pathname !== '/register' &&  window.location.pathname !== '/' &&
-            window.location.pathname !== '/' && <NavBar />}
-      
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/usuario" element={<Usuario />} />
-        <Route path="/clases" element={<Clases />} />
-        <Route path="/panel" element={<Dashboard />} />
-        <Route path="/about-us" element={<AboutUs />} />
-      </Routes>
+      <AppWrapper />
     </Router>
   );
 };
