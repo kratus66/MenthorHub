6780d605c8895e234f79a78dcc5a0e68730d60c9
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-16 23:46:09 +0000

    Add HTTPS configuration to Vite server settings

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 4173
+    port: 4173,
+    https: {
+      key: 'C:/win-acme/certs/mentorhub.info.gf-key.pem',
+      cert: 'C:/win-acme/certs/mentorhub.info.gf-crt.pem'
+    }
   },
   preview: {
     allowedHosts: ['mentorhub.info.gf']
