ef14db27e774b83046666a319886b9bb5b01eb02
Nassim W.H <nwh250@hotmail.com>
2025-05-21 19:23:52 -0400

    Update jwt.strategy.ts

--- back/src/auth/jwt.strategy.ts
+++ back/src/auth/jwt.strategy.ts
@@ -3,22 +3,23 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET')|| 'ment0rhUb_2025_superclave',
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'ment0rhUb_2025_superclave',
     });
+    console.log('JwtStrategy initialized, secret loaded');
   }
 
   async validate(payload: any) {
+    console.log('JwtStrategy -> payload:', payload);
     return {
       id: payload.sub,
       email: payload.email,
-      Role: payload.role,
+      role: payload.role,
     };
   }
 }
