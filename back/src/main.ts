de21fd90f4d3350e9e9f4fc3dcc8e4891bb1ccb3
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-22 18:38:30 -0300

    fix: update server listen method to bind to all network interfaces

--- back/src/main.ts
+++ back/src/main.ts
@@ -47,7 +47,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3001);
+  // await app.listen(3001);
+  await app.listen(3001, '0.0.0.0');
   console.log("Application is running on: http://localhost:3001");
 }
 bootstrap();
