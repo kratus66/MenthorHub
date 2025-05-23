1f783838e2a9d2681cb6056d852f58b1c37b4faa
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-23 10:42:27 -0300

    feat: add AWS deployment workflow and update PowerShell script for backend and frontend deployment

--- deploy.ps1
+++ deploy.ps1
@@ -15,18 +15,18 @@ Set-Location $REPO_DIR
 git pull origin dev
 
 # --- 3. Despliegue del Backend ---
-Write-Output "=== Desplegando Backend ==="
-Set-Location $BACKEND_DIR
+# Write-Output "=== Desplegando Backend ==="
+# Set-Location $BACKEND_DIR
 
-npm install
-npm run build
+# npm install
+# npm run build
 
-# Iniciar backend en una nueva ventana de PowerShell
-# Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm start" -WindowStyle Minimized
-Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm run start:dev" -WindowStyle Minimized
-Write-Output "Backend iniciado en http://localhost:3001"
+# # Iniciar backend en una nueva ventana de PowerShell
+# # Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm start" -WindowStyle Minimized
+# Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm run start:dev" -WindowStyle Minimized
+# Write-Output "Backend iniciado en http://localhost:3001"
 
-Start-Sleep -Seconds 10
+# Start-Sleep -Seconds 10
 
 # --- 4. Despliegue del Frontend ---
 Write-Output "=== Desplegando Frontend ==="
@@ -36,8 +36,8 @@ npm install
 npm run build
 
 # Iniciar frontend en una nueva ventana de PowerShell
-# Start-Process powershell -ArgumentList "-Command", "cd '$FRONTEND_DIR'; npm run preview" -WindowStyle Minimized
-Start-Process powershell -ArgumentList "-Command", "cd '$FRONTEND_DIR'; npm run dev" -WindowStyle Minimized
+Start-Process powershell -ArgumentList "-Command", "cd '$FRONTEND_DIR'; npm run preview" -WindowStyle Minimized
+# Start-Process powershell -ArgumentList "-Command", "cd '$FRONTEND_DIR'; npm run dev" -WindowStyle Minimized
 Write-Output "Frontend iniciado en http://localhost:4173"
 
 Write-Output "=== ¡Despliegue completado! ==="
