51856e1c4957898a196f0411475922715868092f
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-23 08:24:11 -0300

    fix: simplify SSL configuration in DataSource

--- back/src/config/typeorm.ts
+++ back/src/config/typeorm.ts
@@ -14,24 +14,16 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: "postgres",
-
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432", 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: false,
-
   logging: true,
   entities: [User, Class, Task, Submission, Payment, Category, Notification],
   migrations: [__dirname + "/../migrations/*{.ts,.js}"],
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-      sslmode: "require"
-    }
-  },
-  ssl: true
+  ssl: { rejectUnauthorized: false },
 });
 
 export default AppDataSource;
