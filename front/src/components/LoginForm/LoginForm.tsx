6ee410464f43d435d9e4d26a00b27047ee2af906
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-23 03:54:22 -0300

    feat: implement user authentication and improve server checks

--- front/src/components/LoginForm/LoginForm.tsx
+++ front/src/components/LoginForm/LoginForm.tsx
@@ -1,36 +1,54 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
-interface LoginValues {
-  email: string;
-  password: string;
-}
-
-const initialValues: LoginValues = {
-  email: "",
-  password: "",
-};
-
-const validate = (values: LoginValues) => {
-  const errors: Partial<LoginValues> = {};
-
+import { useUser } from "../../context/UserContext";
+import axiosInstance from "../../services/axiosInstance";
+import { useNavigate } from "react-router-dom";
+const initialValues = { email: "", password: "" };
+const validate = (values: typeof initialValues) => {
+  const errors: Partial<typeof initialValues> = {};
   if (!values.email) {
     errors.email = "El email es requerido";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
     errors.email = "Email inválido";
   }
-
   if (!values.password) {
     errors.password = "La contraseña es requerida";
   } else if (values.password.length < 6) {
     errors.password = "Mínimo 6 caracteres";
   }
-
   return errors;
 };
-
 export default function LoginForm() {
-  const handleSubmit = () => {};
-
+  const { login } = useUser();
+  const navigate = useNavigate();
+  const handleSubmit = async (
+    values: typeof initialValues,
+    { setErrors }: any
+  ) => {
+    try {
+      const response = await axiosInstance.post("/auth/login", values);
+      if (!response.data.token) {
+        setErrors({
+          email: "Credenciales no válidas",
+          password: "Credenciales no válidas",
+        });
+        return;
+      }
+      login(response.data.user, response.data.token);
+      navigate("/panel");
+    } catch (error: any) {
+      if (error.status === 400) {
+        setErrors({
+          email: "Credenciales no válidas",
+          password: "Credenciales no válidas",
+        });
+      } else if (error.status === 401) {
+        setErrors({ email: "Email sin verificar" });
+        // Ver volver a enviar solicitud de verificación
+      } else {
+        setErrors({ email: "Error de Conexion" });
+      }
+    }
+  };
   return (
     <Formik
       initialValues={initialValues}
@@ -58,7 +76,6 @@ export default function LoginForm() {
             />
           </div>
         </div>
-
         <div className="relative mx-[2vw]">
           <label htmlFor="password" className="block font-medium">
             Contraseña
@@ -78,7 +95,9 @@ export default function LoginForm() {
             />
           </div>
         </div>
-        <a className="text-right mx-[2vw] text-[#007AFF] hover:underline underline-offset-2">¿Has olvidado tu contraseña?</a>
+        <a className="text-right mx-[2vw] text-[#007AFF] hover:underline underline-offset-2">
+          ¿Has olvidado tu contraseña?
+        </a>
         <button
           type="submit"
           className="w-full bg-[#007AFF] text-white mx-auto py-3 px-6 rounded-full hover:bg-white hover:text-[#007AFF] border-2 border-[#007AFF]"
--- front/src/services/axiosInstance.ts
+++ front/src/services/axiosInstance.ts
@@ -1,10 +1,13 @@
 // src/api/axiosInstance.js
-import axios from 'axios';
+import axios from "axios";
 
 const apiUrl: string = import.meta.env.VITE_API_URL || "localhost:3001";
 const axiosInstance = axios.create({
-    baseURL: `${ window.location.protocol }//${apiUrl}/api`, // Reemplazá con la URL de tu backend si es otra
-    // También podés agregar otras configs si querés
+  baseURL: `${window.location.protocol}//${apiUrl}/api`, // Reemplazá con la URL de tu backend si es otra
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // También podés agregar otras configs si querés
 });
 
 // Ejemplo opcional de interceptor de request para token
