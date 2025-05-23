a1966941ec9973ecc7300c4a632530e827c39579
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-23 10:59:55 -0300

    Refactor deployment script to comment out unused backend deployment steps and update Git pull command

--- deploy.ps1
+++ deploy.ps1
@@ -5,26 +5,28 @@ $FRONTEND_DIR = "$REPO_DIR\front"
 
 # --- 1. Detener todos los procesos Node existentes ---
 Write-Output "=== Deteniendo procesos anteriores ==="
-taskkill /F /IM node.exe /T 2> $null
 kill-port 3001 4173
+taskkill /F /IM node.exe /T 2> $null
 
 # --- 2. Actualizar repositorio ---
 Write-Output "=== Actualizando código desde GitHub ==="
 Set-Location $REPO_DIR
-git pull origin main
+# git pull origin main
+git pull origin dev
 
 # --- 3. Despliegue del Backend ---
-Write-Output "=== Desplegando Backend ==="
-Set-Location $BACKEND_DIR
+# Write-Output "=== Desplegando Backend ==="
+# Set-Location $BACKEND_DIR
 
-npm install
-npm run build
+# npm install
+# npm run build
 
-# Iniciar backend en una nueva ventana de PowerShell
-Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm start" -WindowStyle Minimized
-Write-Output "Backend iniciado en http://localhost:3001"
+# # Iniciar backend en una nueva ventana de PowerShell
+# # Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm start" -WindowStyle Minimized
+# Start-Process powershell -ArgumentList "-Command", "cd '$BACKEND_DIR'; npm run start:dev" -WindowStyle Minimized
+# Write-Output "Backend iniciado en http://localhost:3001"
 
-Start-Sleep -Seconds 10
+# Start-Sleep -Seconds 10
 
 # --- 4. Despliegue del Frontend ---
 Write-Output "=== Desplegando Frontend ==="
@@ -35,6 +37,7 @@ npm run build
 
 # Iniciar frontend en una nueva ventana de PowerShell
 Start-Process powershell -ArgumentList "-Command", "cd '$FRONTEND_DIR'; npm run preview" -WindowStyle Minimized
+# Start-Process powershell -ArgumentList "-Command", "cd '$FRONTEND_DIR'; npm run dev" -WindowStyle Minimized
 Write-Output "Frontend iniciado en http://localhost:4173"
 
 Write-Output "=== ¡Despliegue completado! ==="
