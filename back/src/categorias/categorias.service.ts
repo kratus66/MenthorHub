439c25e721d77aec9f733e1ba625c975f6188f45
Alfonso <alfonso.gonzalezwebdev@gmail.com>
2025-05-24 12:47:58 -0300

    intentando hacer la paginacion

--- back/src/categorias/categorias.service.ts
+++ back/src/categorias/categorias.service.ts
@@ -6,27 +6,28 @@ import { CreateCategoryDto } from '../dto/create-category.dto';
 
 @Injectable()
 export class CategoriesService {
-  constructor(
-    @InjectRepository(Category)
-    private categoryRepo: Repository<Category>,
-  ) {}
+   constructor(
+      @InjectRepository(Category)
+      private categoryRepo: Repository<Category>
+   ) {}
 
-  async create(dto: CreateCategoryDto) {
-    const category = this.categoryRepo.create({
-      name: dto.name,
-      imageUrl: dto.imageUrl,
-      description: dto.description,
-    });
-    return await this.categoryRepo.save(category);
-  }
+   async create(dto: CreateCategoryDto) {
+      const category = this.categoryRepo.create({
+         name: dto.name,
+         imageUrl: dto.imageUrl,
+         description: dto.description,
+      });
+      return await this.categoryRepo.save(category);
+   }
 
-  async findAll() {
-    return await this.categoryRepo.find();
-  }
+   async findAll() {
+      return await this.categoryRepo.find({
+         relations: ['materias'],
+      });
+   }
 
-  async createMany(data: CreateCategoryDto[]) {
-    const categories = this.categoryRepo.create(data);
-    return await this.categoryRepo.save(categories);
-  }
+   async createMany(data: CreateCategoryDto[]) {
+      const categories = this.categoryRepo.create(data);
+      return await this.categoryRepo.save(categories);
+   }
 }
-
