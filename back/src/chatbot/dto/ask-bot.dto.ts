5205b6da0eed4689a528155797e189321b2b46c4
Nassim Wessin <nassimwhazim@gmail.com>
2025-05-28 11:08:14 -0400

    Delete back/src directory

--- back/src/chatbot/dto/ask-bot.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class AskBotDto {
-  @IsString()
-  message: string = '';
-}
--- back/src/dto/create-category.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateCategoryDto {
-  @ApiProperty({ example: 'Salud', description: 'Nombre de la categoría' })
-  @IsString()
-  name: string;
-}
