707aa6359418d04ac555c18328a5db2a25273a2c
Nassim Wessin <nassimwhazim@gmail.com>
2025-05-28 11:08:14 -0400

    Delete back/src directory

--- back/src/chatbot/chatbot.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatbotService } from './chatbot.service';
-import { ChatbotController } from './chatbot.controller';
-
-@Module({
-  controllers: [ChatbotController],
-  providers: [ChatbotService],
-})
-export class ChatbotModule {}
