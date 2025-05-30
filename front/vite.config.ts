08b8d95d24099a4db392f897e699ce8da8531fe4
Francisco Beese <alexandrobeese@hotmail.com>
2025-05-30 09:20:41 -0300

    New deploy setup

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,35 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import type { UserConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
+import path from "path";
 
 export default defineConfig(({ mode }): UserConfig => {
-   const config: UserConfig = {
-      plugins: [react()],
-      resolve: {
-         alias: {
-            '@': path.resolve(__dirname, './src'),
-         },
+  const config: UserConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-      server: {
-         host: '0.0.0.0',
-         port: 4173,
-         allowedHosts: ['mentorhub.info.gf'],
-      },
-      preview: {
-         allowedHosts: ['mentorhub.info.gf'],
-      },
-   };
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 4173,
+      allowedHosts: ["mentorhub.info.gf"],
+    },
+    preview: {
+      allowedHosts: ["mentorhub.info.gf"],
+    },
+  };
 
-   if (mode === 'production') {
-      config.server = {
-         ...config.server,
-         https: {
-            key: 'C:/win-acme/certs/mentorhub.info.gf-key.pem',
-            cert: 'C:/win-acme/certs/mentorhub.info.gf-crt.pem',
-         },
-      };
-   }
+  if (mode === "production") {
+    config.server = {
+      ...config.server,
+      https: {
+        key: "/etc/letsencrypt/live/mentorhub.info.gf/privkey.pem",
+        cert: "/etc/letsencrypt/live/mentorhub.info.gf/fullchain.pem",
+      },
+    };
+  }
 
-   return config;
+  return config;
 });
