996ca1d8b1e3245c1d6d6ae14725d0bc529639d3
Francisco Beese <alexandrobeese@hotmail.com>
2025-06-02 16:58:20 -0300

    Add token header to axios instance for authentication

--- front/src/services/axiosInstance.ts
+++ front/src/services/axiosInstance.ts
@@ -6,6 +6,7 @@ const axiosInstance = axios.create({
    baseURL: `${window.location.protocol}//${apiUrl}/api`, // Reemplazá con la URL de tu backend si es otra
    headers: {
       'Content-Type': 'application/json',
+      token: localStorage.getItem('token') || '', // Agrega el token de autenticación si está disponible
    },
    // También podés agregar otras configs si querés
 });
